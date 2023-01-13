3206987dd8131028960ad6d740f5ae354820f1a8
okom kamara <okomkamara422@hotmail.com>
2023-01-13 12:20:19 -0800

    Add Features

--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+  const App = () =>  {
+    const [ quotes, setQuotes ] = useState([]);
+    const [ randomQuote, setRandomQuote ] = useState("");
+    const [ color, setColor ] = useState("#111")
+
+     useEffect(() => {
+      async function fetchData() {
+      try {
+        const response =  await fetch("https://type.fit/api/quotes");
+        const data = await response.json();
+
+        setQuotes(data)
+        let randomIndex = Math.floor(Math.random() * data.length)
+        setRandomQuote(data[randomIndex])
+      } catch(error) {
+        console.log(error);
+        setQuotes(error)
+      }
+}
+   fetchData()
+     }, [])
+     
+     const getNewQuote = () => {
+      const colors = [
+          "#16a085",
+          "#27ae60",
+          "#2c3e50",
+          "#f39c12",
+          "#e74c3c",
+          "#9b59b6",
+          "#FB6964",
+          "#342224",
+          "#472E32",
+          "#BDBB99",
+          "#77B1A9",
+          "#73A857",
+        ];
+        let randomIndex = Math.floor(Math.random() * quotes.length);
+        let randomColorIndex = Math.floor(Math.random() * colors.length);
+        setRandomQuote(quotes[randomIndex]);
+        setColor(colors[randomColorIndex])
+      }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: color, minHeight: "100vh"}}>
+    <div className="container pt-5">
+      <div className="jumbotron">
+        <div className="card">
+          <div className="card-header">Inspirational Quotes</div>
+          <div className='card-body'>
+            {randomQuote ? (
+              <>
+              <h5 className='card-title'>
+                -{randomQuote.author || "No author"}
+              </h5>
+              <p className='card-text'>&quot;{randomQuote.text}&squot;</p>
+              </>
+            ) : (
+           <h2>Loading</h2>
+            )}
+            <div className="row">
+              <div className="col">
+                <button onClick={getNewQuote} className="btn btn-primary" >
+                  New Quote
+                  </button>
+                  <a href= {
+                     "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" +
+                     encodeURIComponent(
+                       '"' + randomQuote.text + '" ' + randomQuote.author
+                     )
+                  }
+                  className="btn btn-warning"
+                  >
+                      <i className='fa fa-twitter'></i>
+                    
+                    </a>
+                    <a
+                  href={
+                    "https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=" +
+                    encodeURIComponent(randomQuote.author) +
+                    "&content=" +
+                    encodeURIComponent(randomQuote.text) +
+                    "&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button"
+                  }
+                  
+                  className="btn btn-danger"
+                >
+                  <i className="fa fa-tumblr"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
