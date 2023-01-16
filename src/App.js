9520bf5de4dc167069cf6ffa8c81953fb6885702
okom kamara <okomkamara422@hotmail.com>
2023-01-16 09:30:44 -0800

    add feature in App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
+// Raandom quote machine
 import { useEffect, useState } from 'react';
 import './App.css';
 
   const App = () =>  {
     const [ quotes, setQuotes ] = useState([]);
     const [ randomQuote, setRandomQuote ] = useState("");
-    const [ color, setColor ] = useState("#111")
+     const [ color, setColor ] =  useState("#111");
 
      useEffect(() => {
       async function fetchData() {
@@ -50,55 +50,67 @@ import './App.css';
     <div className="container pt-5">
       <div className="jumbotron">
         <div className="card">
-          <div className="card-header">Inspirational Quotes</div>
+          <div className="container text-center">Inspirational Quotes</div>
           <div className='card-body'>
             {randomQuote ? (
               <>
               <h5 className='card-title'>
                 -{randomQuote.author || "No author"}
               </h5>
-              <p className='card-text'>&quot;{randomQuote.text}&squot;</p>
+              <p className='card-text'>&quot;{randomQuote.text}&quot;</p>
               </>
             ) : (
            <h2>Loading</h2>
             )}
-            <div className="row">
-              <div className="col">
+          
+             
+                <div className='row'>
+                  <div className='col'>
                 <button onClick={getNewQuote} className="btn btn-primary" >
                   New Quote
                   </button>
-                  <a href= {
-                     "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" +
-                     encodeURIComponent(
-                       '"' + randomQuote.text + '" ' + randomQuote.author
-                     )
-                  }
-                  className="btn btn-warning"
-                  >
-                      <i className='fa fa-twitter'></i>
-                    
-                    </a>
-                    <a
-                  href={
-                    "https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=" +
-                    encodeURIComponent(randomQuote.author) +
-                    "&content=" +
-                    encodeURIComponent(randomQuote.text) +
-                    "&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button"
-                  }
                   
-                  className="btn btn-danger"
-                >
-                  <i className="fa fa-tumblr"></i>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    </div>
-  );
+                <div className='row justify-content-end'>
+                  <div className='col'>
+                  <a
+              href= {
+                "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" +
+                encodeURIComponent(
+                  '"' + randomQuote.text + '" ' + randomQuote.author
+                )
+             }
+             className="btn btn-warning"
+             >
+                 <i className='fa fa-twitter'></i>
+               
+              </a>
+               <a
+             href={
+               "https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=" +
+               encodeURIComponent(randomQuote.author) +
+               "&content=" +
+               encodeURIComponent(randomQuote.text) +
+               "&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button"
+             }
+             
+             className="btn btn-danger"
+           >
+             <i className="fa fa-tumblr"></i>
+           </a>
+           </div>
+           </div>
+           </div>
+         </div>
+       </div>
+     </div>
+   </div>
+ </div>
+</div>
+);
 }
 
 export default App;
+              
+                
+              
+
